724766d6c338ca17dcf1cf8b68aed563f1de59c9
apollosport <apollosport84@gmail.com>
2022-11-17 17:38:40 +0100

    i dont remember

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [posts, setPosts] = useState([]);
   const [onLoading, setOnloading] = useState(false);
   const [url, setUrl] = useState(
-    "http://hn.algolia.com/api/v1/search?tags=front_page"
+    "http://hn.algolia.com/api/v1/search?tags=front_page&hitsPerPage=50"
   );
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(5);
@@ -57,16 +57,20 @@ function App() {
           <SpinnerCircular />
         </div>
       ) : (
-<<<<<<< HEAD
-        <News posts={posts} setUrl={setUrl} />
-=======
-        <News posts={currentPosts} />
->>>>>>> 243fe1ff9f944d96ea10b81e68f9d8c38c7150dc
+        <News
+          posts={currentPosts}
+          setUrl={setUrl}
+          currentPage={currentPage}
+          postsPerPage={postsPerPage}
+        />
       )}
       <Pagination
         totalPosts={posts.length}
         postsPerPage={postsPerPage}
         paginate={paginate}
+        setPostsPerPage={setPostsPerPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
       />
       <Footer />
     </div>
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,10 +1,10 @@
 import "./News.css";
 import Post from "./Posts/Post";
-export default function News({ posts, setUrl }) {
+export default function News({ posts, setUrl, currentPage, postsPerPage }) {
   /*  console.log("posts in News:", posts); */
   return (
     <div className="News">
-      <ol className="News-ol">
+      <ol className="News-ol" start={(currentPage - 1) * postsPerPage + 1}>
         {posts.length === 0 ? (
           <div className="noData">No data Found</div>
         ) : (
